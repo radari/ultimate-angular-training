8f3cd20094bd0ceaf0cd5734dffb76a356cd2806
ykarhoo <ykarhoo@cirb.brussels>
2018-05-09 11:53:30 +0200

    Added rest parameter examples

--- src/restParameterSample.ts
+++ src/restParameterSample.ts
@@ -1,6 +1,13 @@
 function concat(word, ... arrayOfWords) {
-    console.log(arguments);
+    logArguments(word, arrayOfWords);
     return word + arrayOfWords.reduce((prev, current) => prev + current);
 }
 
+function logArguments(word, ... arrayOfWords) {
+    console.log(arguments);
+}
+
 console.log(concat("Hello", 'My', 'Name', 'Is', 'Charly'));
+logArguments("Hello");
+logArguments("Hello", null);
+logArguments("Hello", undefined);
