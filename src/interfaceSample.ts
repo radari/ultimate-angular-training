1840b3e1cdf25b96d885a563f0b48db8dff487ad
khyiu <superk888@gmail.com>
2018-05-13 16:51:31 +0200

    optional properties in interface

--- src/interfaceSample.ts
+++ src/interfaceSample.ts
@@ -3,6 +3,7 @@
         brand: string,
         model: string,
         fuel: 'gas' | 'diesel' | 'electric',
+        maxSpeed?: number,
 
         getName(): string
     }
@@ -27,12 +28,13 @@
         model: 'P Serie',
         fuel: 'diesel',
         maxPayload: 25000,
+        maxSpeed: 164,
 
         getName: function() {
             return `${this.brand} ${this.model} truck - max payload = ${this.maxPayload}`;
         }
     };
 
-    console.log(myCar.getName());
-    console.log(myTruck.getName());
+    console.log(myCar.getName(), myCar.maxSpeed);
+    console.log(myTruck.getName(), myTruck.maxSpeed);
 }
