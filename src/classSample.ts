90a1e5fb9e95852b4c84bd35276265927f6401fe
khyiu <superk888@gmail.com>
2018-05-14 16:19:50 +0200

    readonly modifier

--- src/classSample.ts
+++ src/classSample.ts
@@ -6,7 +6,7 @@
 
         // note: in constructor arguments, when specifying a public/private modifier, we are actually using a shorthanded attribute definition
         //       --> no need to explicitly define it at class level...
-        constructor(brand: string, model: string, private version: string, public color: string) {
+        constructor(brand: string, model: string, private version: string, public color: string, readonly vin:string) {
             this.brand = brand;
             this.model = model;
         }
@@ -20,11 +20,12 @@
         }
     }
 
-    let myCar: Car = new Car('McLaren', 'P1', 'x', 'Stellar blue');
+    let myCar: Car = new Car('McLaren', 'P1', 'x', 'Stellar blue', 'ZAPC16C1435072584');
     myCar.addFeatures('satnav');
     myCar.addFeatures('metallic painting');
     console.log(myCar);
     myCar.clearFeatures();
     console.log(myCar);
     console.log(myCar.color);
+    console.log(myCar.vin);
 }
