c3764038e4f19401da13caf66d2d7a623a74218b
khyiu <superk888@gmail.com>
2018-05-14 16:06:10 +0200

    public/private modifier + shorthanded class attribute definition

--- src/classSample.ts
+++ src/classSample.ts
@@ -4,7 +4,9 @@
         model: string;
         features: string[] = [];
 
-        constructor(brand: string, model: string) {
+        // note: in constructor arguments, when specifying a public/private modifier, we are actually using a shorthanded attribute definition
+        //       --> no need to explicitly define it at class level...
+        constructor(brand: string, model: string, private version: string, public color: string) {
             this.brand = brand;
             this.model = model;
         }
@@ -18,10 +20,11 @@
         }
     }
 
-    let myCar: Car = new Car('McLaren', 'P1');
+    let myCar: Car = new Car('McLaren', 'P1', 'x', 'Stellar blue');
     myCar.addFeatures('satnav');
     myCar.addFeatures('metallic painting');
     console.log(myCar);
     myCar.clearFeatures();
     console.log(myCar);
+    console.log(myCar.color);
 }
